ce7eeabb19317e42d3a26e0195fe8c94eed6e050
Florian68240 <merckle.florian@orange.fr>
2023-06-03 15:01:42 +0200

    v

--- magasins_jeu_video/jeux_video/urls.py
+++ magasins_jeu_video/jeux_video/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from . import views
 
+from django.urls import path
+from . import views
+from . import livreviews
+
 urlpatterns = [
     path('index/', views.index),
     path('ajout/', views.ajout),
-    path('formulaire/', views.formulaire),
+    path('traitement/', views.traitement),
+    path('affiche/<int:id>/', views.affiche),
+    path('delete/<int:id>/', views.delete),
+    path('update/<int:id>/', views.update),
 ]
